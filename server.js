fa2f3a3a3e4ba164289838de6f4ab2306e15787f
mrmerarif <bflores2@byupathway.edu>
2026-03-13 00:09:41 -0600

    updated files for week 2

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+
+
+
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -51,7 +55,13 @@ app.get('/', async (req, res) => {
     res.render('categories', { title });
   });
   
-  app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+  app.listen(PORT, async () => {
+    try {
+      await testConnection();
+      console.log(`Server is running at http://127.0.0.1:${PORT}`);
+      console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+    }
   });
+  
